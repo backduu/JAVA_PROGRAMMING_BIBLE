4946031ce4c8ba8131a623e4843c83972ec8587a
BackDoo <power4290@gmail.com>
2023-10-31 15:45:03 +0900

    제네릭 메소드와 와일드카드
    + doc 업데이트..추후 집에 있는 것들과 통합해버리기.

--- NoteForJAVA/src/main/java/org/example/Generic01.java
+++ NoteForJAVA/src/main/java/org/example/Generic01.java
@@ -2,23 +2,35 @@ package org.example;
 
 import java.util.ArrayList;
 
+class Animals {
+    public void crying() {
+        System.out.println("동물들이 울고 있어!");
+    }
+}
+
+class Cat extends Animals {
+    public void crying() {
+        System.out.println("야옹~~~");
+    }
+}
+class Dog extends Animals {
+    public void crying() {
+        System.out.println("멍멍!");
+    }
+}
+
 public class Generic01<T> {
     ArrayList<T> list = new ArrayList<>();
-
+    public static void cryingAnimalList(Generic01<? extends Animals> items) {
+        Animals a = items.get(0);
+        a.crying();
+    }
     void add(T item) {
         list.add(item);
     }
 
     T get(int idx) {
         return list.get(idx);
-    }
-
-    boolean remove(T item) {
-        return list.remove(item);
-    }
-
-    int size() {
-        return list.size();
     }
 
     void show() {
--- NoteForJAVA/src/main/java/org/example/Main.java
+++ NoteForJAVA/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Generic01<String> generic01 = new Generic01<>();
+        /*Generic01<String> generic01 = new Generic01<>();
 
         generic01.add("item1");
         generic01.add("item2");
@@ -11,6 +11,14 @@ public class Main {
         Generic01<Integer> generic02 = new Generic01<>();
         generic02.add(43);
         generic02.add(5425);
-        generic02.show();
+        generic02.show();*/
+
+        // 와일드카드
+        Generic01<Cat> cat = new Generic01<>();
+        cat.add(new Cat());
+        Generic01<Dog> dog = new Generic01<>();
+        dog.add(new Dog());
+        Generic01.cryingAnimalList(cat);
+        Generic01.cryingAnimalList(dog);
     }
 }
